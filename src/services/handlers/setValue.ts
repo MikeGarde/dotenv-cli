c7870f7536f697fa0524d27eff24b5a3a6e6ea68
Mike Garde <mgarde@gmail.com>
2024-07-03 00:41:18 -0400

    Consolidating options
    Also moving Qualifying Rules out of app.ts

--- src/services/handlers/setValue.ts
+++ src/services/handlers/setValue.ts
@@ -1,35 +1,34 @@
-import fs             from "node:fs";
-import log            from "../../log.js";
-import EnvObject      from "../../envObject.js";
-import escapeAndQuote from "../../escapeAndQuote.js";
+import fs        from "node:fs";
+import log       from "../../log.js";
+import {Options} from "../../components/qualifyingRules.js";
 
-export default function setValue(envObject: EnvObject, envFilePath: string, key: string, setValue: string, quoteSet: boolean) {
-  const newValue: string = escapeAndQuote(setValue, quoteSet);
-  const newLines: string = `${key}=${newValue}`;
+export default function setValue(options: Options) {
+  const key: string      = options.targetKeys[0];
+  const newLines: string = `${key}=${options.setValue}`;
 
   log.debug(`Updating "${key}"`);
 
   // Do we want to update or append the .env file?
-  if (envObject[key]) {
-    log.debug('Updating existing key', envObject[key]);
-    const lineStart = envObject[key].lineStart;
-    const lineEnd   = envObject[key].lineEnd;
+  if (options.envObject[key]) {
+    log.debug('Updating existing key', options.envObject[key]);
+    const lineStart = options.envObject[key].lineStart;
+    const lineEnd   = options.envObject[key].lineEnd;
     log.debug(`Replacing lines ${lineStart}-${lineEnd}`);
 
     // Split the new lines into an array
     let newLinesArray: string[] = newLines.split('\n');
 
     // Read the file and split it into an array of lines
-    let lines: string[] = fs.readFileSync(envFilePath, 'utf8').split('\n');
+    let lines: string[] = fs.readFileSync(options.fullEnvPath, 'utf8').split('\n');
 
     // Replace the lines between lineStart and lineEnd
     lines.splice(lineStart, lineEnd - lineStart + 1, ...newLinesArray);
 
     // Join the lines back together and write the result back to the file
-    fs.writeFileSync(envFilePath, lines.join('\n'));
+    fs.writeFileSync(options.fullEnvPath, lines.join('\n'));
   } else {
-    log.debug(`Appending "${key}" to "${envFilePath}"`);
+    log.debug(`Appending "${key}" to "${options.fullEnvPath}"`);
 
-    fs.writeFileSync(envFilePath, `${newLines}\n`, {flag: 'a'});
+    fs.writeFileSync(options.fullEnvPath, `${newLines}\n`, {flag: 'a'});
   }
 };
