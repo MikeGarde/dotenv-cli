2fee02a8faa6adffee8cf38a103e3b68253d6c70
Mike Garde <mgarde@gmail.com>
2025-06-10 20:16:19 -0500

    npm update
    - Updated tsconfig.json:
      - Changed "module" setting to "ESNext"
      - Updated "moduleResolution" to "Node"
      - Set "target" to "ES2022"
      - Added "allowJs" and "resolveJsonModule" options
    - Refined include paths to "src/**/*.ts" for TypeScript files
    - Updated and improved configuration settings for better compatibility and flexibility

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset          : 'ts-jest',
-  testEnvironment : 'node',
-  testMatch       : ['<rootDir>/tests/**/*.tests.ts'],
-  moduleNameMapper: {
-    '(.+)\\.js': '$1', // Remove the .js extension
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testMatch: ['<rootDir>/tests/**/*.tests.ts'],
   verbose: true,
 };
